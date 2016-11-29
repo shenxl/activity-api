f922e98d5e6a6fc1af7846ec6b262f0432168204
shenxiaolei <shenxiaolei@kingsoft.com>
2016-11-29 12:45:42 +0800

    配合完善 CURD 系列操作

--- common/models/company_monthly.js
+++ common/models/company_monthly.js
@@ -4,5 +4,14 @@ var _ = require('lodash');
 
 
 module.exports = function(companyMonthly) {
-  
+  companyMonthly.remoteMethod('destroyAll', {
+    isStatic: true,
+    description: 'Delete all matching records',
+    accessType: 'WRITE',
+    accepts: {arg: 'where', type: 'object', description: 'filter.where object'},
+    http: {verb: 'del', path: '/'},
+    returns: {
+        arg: 'success', type: 'object', root: true
+    },
+  });
 };
---
+++ common/models/demo.js
@@ -0,0 +1,11 @@
+// Copyright IBM Corp. 2014,2015. All Rights Reserved.
+// Node module: loopback-example-user-management
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+var config = require('../../server/config.json');
+var path = require('path');
+
+module.exports = function(Demo) {
+
+};
